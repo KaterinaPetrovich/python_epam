a442bcbd4a16159508239bb0757783b9739f8409
KaterinaPetrovich <katerina_petrovich@bk.ru>
2021-11-08 15:47:20 +0300

    task2 completed

---
+++ homework1/task2.py
@@ -0,0 +1,16 @@
+from typing import Sequence
+
+
+def check_fibonacci(data: Sequence[int]) -> bool:
+    if len(data) == 0:
+        return False
+    fib = generate_fib(len(data))
+    return fib == data
+
+
+def generate_fib(n: int) -> Sequence[int]:
+    if n == 1:
+        return [0]
+    fib = [0, 1]
+    fib = [fib[i - 1] + fib[i - 2] for i in range(2, n)]
+    return fib
