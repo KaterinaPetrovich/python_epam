6d2aa5a00459155e861006bffa71c5fd69c6dac8
KaterinaPetrovich <katerina_petrovich@bk.ru>
2021-11-09 00:37:27 +0300

    fixed imports

--- tests/homework1/test_task4.py
+++ tests/homework1/test_task4.py
@@ -1,5 +1,5 @@
-from homework1.task4 import check_sum_of_four
+from homework1.task4 import check_four
 
 
-def test_check_sum_of_four():
-    assert check_sum_of_four([1, 2], [1, -2], [0, 2], [-1, 0]) == 2
+def test_check_four():
+    assert check_four([1, 2], [1, -2], [0, 2], [-1, 0]) == 2
