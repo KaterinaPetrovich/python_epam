5a485f5ec2da7bf8efac067863c9093d90eb9e18
KaterinaPetrovich <katerina_petrovich@bk.ru>
2021-11-17 18:17:56 +0300

    file path changed

--- tests/homework2/test_task1.py
+++ tests/homework2/test_task1.py
@@ -4,20 +4,20 @@ from homework2.task1 import (count_non_ascii_chars, count_punctuation_chars,
 
 
 def test_get_longest_diverse_words():
-    assert "unmißverständliche" in get_longest_diverse_words("data.txt")
+    assert "unmißverständliche" in get_longest_diverse_words("./homework2/data.txt")
 
 
 def test_get_rarest_char():
-    assert "’" in get_rarest_char("data.txt")
+    assert "’" in get_rarest_char("./homework2/data.txt")
 
 
 def test_count_punctuation_chars():
-    assert count_punctuation_chars("data.txt") > 5400
+    assert count_punctuation_chars("./homework2/data.txt") > 5400
 
 
 def test_count_non_ascii_chars():
-    assert count_non_ascii_chars("data.txt") > 2900
+    assert count_non_ascii_chars("./homework2/data.txt") > 2900
 
 
 def test_get_most_common_non_ascii_char():
-    assert get_most_common_non_ascii_char("data.txt") == ["ä"]
+    assert get_most_common_non_ascii_char("./homework2/data.txt") == ["ä"]
