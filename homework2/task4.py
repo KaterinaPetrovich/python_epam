7883af9f507a972b6eeac244dac6cfd673b604e4
KaterinaPetrovich <katerina_petrovich@bk.ru>
2021-11-22 02:15:35 +0300

    qwerty

--- homework2/task4.py
+++ homework2/task4.py
@@ -4,11 +4,27 @@ from typing import Callable
 def cache(func: Callable) -> Callable:
     memo = {}
 
-    def wrapper(*args):
+    def wrapper(*args, **kwargs):
         try:
             return memo[args]
         except KeyError:
-            memo[args] = func(*args)
+            memo[args] = func(*args, **kwargs)
             return memo[args]
 
     return wrapper
+
+
+def function():
+    return input('? ')
+
+cache_func = cache(function)
+'''
+cache_func()
+print("g")
+cache_func()
+print("b")
+cache_func()
+'''
+
+cache_func()
+cache_func()
