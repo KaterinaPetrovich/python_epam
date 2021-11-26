891a76e2a93eb0d231e6235118b9bf11792fbe9e
KaterinaPetrovich <katerina_petrovich@bk.ru>
2021-11-27 02:02:01 +0300

    task1

--- homework4/task1.py
+++ homework4/task1.py
@@ -1,7 +1,7 @@
 def read_magic_number(path: str) -> bool:
     try:
         with open(path) as file:
-            number = float(file.readline(1))
+            number = float(file.readline())
     except ValueError as val_err:
         raise val_err
     return 1 <= number < 3
