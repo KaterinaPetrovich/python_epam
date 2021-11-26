6ada952ea8485e036b75fa17bc52353775b8b93d
KaterinaPetrovich <katerina_petrovich@bk.ru>
2021-11-27 01:22:43 +0300

    add tests task1

--- tests/homework4/test_task1.py
+++ tests/homework4/test_task1.py
@@ -1,10 +1,24 @@
 import tempfile
 
+import pytest
+
 from homework4.task1 import read_magic_number
 
 
-def test_read_magic_number():
-    with tempfile.NamedTemporaryFile(mode='w+t') as temp:
-        temp.write("2")
-        temp.seek(0)
-        assert read_magic_number(temp.name)
+def test_positive_read_magic_number():
+    with tempfile.NamedTemporaryFile(mode='w+t') as fp:
+        fp.write("2")
+        assert read_magic_number(fp.name)
+
+
+def test_string_read_magic_number():
+    with tempfile.NamedTemporaryFile(mode='w+t') as fp:
+        fp.write("asdfg")
+        with pytest.raises(ValueError):
+            read_magic_number(fp.name)
+
+
+def test_out_of_interval_read_magic_number():
+    with tempfile.NamedTemporaryFile(mode='w+t') as fp:
+        fp.write("14")
+        assert not read_magic_number(fp.name)
