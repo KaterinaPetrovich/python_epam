e2be7566d78ba1f798884c6896b76229f2c292cd
KaterinaPetrovich <katerina_petrovich@bk.ru>
2021-11-28 00:50:34 +0300

    task2

---
+++ tests/homework4/test_task2.py
@@ -0,0 +1,25 @@
+from unittest.mock import Mock, patch
+
+import pytest
+import requests
+
+from homework4.task2 import count_dots_on_i
+
+
+def test_positive_count_dots_on_i():
+    test_data = "tyuiortyuiopoiuyt"
+
+    def fake_get(url):
+        fake_response = Mock()
+        fake_response.text = test_data
+        return fake_response
+
+    with patch("requests.get", fake_get):
+        assert count_dots_on_i("https://example.org") == 3
+
+
+def test_error_count_dots_on_i():
+    fake_get = Mock(side_effect=ValueError())
+    with patch("requests.get", fake_get):
+        with pytest.raises(ValueError):
+            count_dots_on_i("https://example.org")
