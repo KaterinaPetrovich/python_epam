2a8a6063aac568191a5b93d06ed09c668bdf460b
KaterinaPetrovich <katerina_petrovich@bk.ru>
2021-11-28 17:30:43 +0300

    task3

---
+++ tests/homework4/test_task3.py
@@ -0,0 +1,19 @@
+import sys
+
+from homework4.task3 import my_precious_logger
+
+
+def test_error_my_precious_logger(capsys):
+    text = "error some text"
+    my_precious_logger(text)
+    out, err = capsys.readouterr()
+    assert err == text
+    assert out == ""
+
+
+def test_out_my_precious_logger(capsys):
+    text = "some text"
+    my_precious_logger(text)
+    out, err = capsys.readouterr()
+    assert err == ""
+    assert out == text
