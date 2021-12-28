de8f2b267a662f7a5c3b172bbfb1aeec75035126
KaterinaPetrovich <katerina_petrovich@bk.ru>
2021-12-29 02:33:21 +0300

    task3

--- tests/homework9/test_task3.py
+++ tests/homework9/test_task3.py
@@ -2,8 +2,8 @@ from homework9.task3 import universal_file_counter
 
 
 def test_universal_file_counter_without_tokenizer():
-    assert universal_file_counter("./homework8", "txt") == 4
+    assert universal_file_counter("./homework8/", "txt") == 4
 
 
 def test_universal_file_counter_with_tokenizer():
-    assert universal_file_counter("./homework8", "txt", str.split) == 4
+    assert universal_file_counter("./homework8/", "txt", str.split) == 4
