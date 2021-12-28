07924051fefbe381e3b9ae3ed1279625eef38767
KaterinaPetrovich <katerina_petrovich@bk.ru>
2021-12-29 02:23:09 +0300

    test task3

--- tests/homework9/test_task3.py
+++ tests/homework9/test_task3.py
@@ -4,28 +4,8 @@ from homework9.task3 import universal_file_counter
 
 
 def test_universal_file_counter_without_tokenizer():
-    with tempfile.NamedTemporaryFile(
-            mode="w+t", dir="./homework9", suffix="txt"
-    ) as fp:
-        with tempfile.NamedTemporaryFile(
-            mode="w+t", dir="./homework9", suffix="txt"
-        ) as ft:
-            fp.write("1 s s\n4")
-            fp.seek(0)
-            ft.write("2\n5")
-            ft.seek(0)
-            assert universal_file_counter("./homework9", "txt") == 4
+    assert universal_file_counter("./homework8", "txt") == 4
 
 
 def test_universal_file_counter_with_tokenizer():
-    with tempfile.NamedTemporaryFile(
-            mode="w+t", dir="./homework9", suffix="txt"
-    ) as fp:
-        with tempfile.NamedTemporaryFile(
-            mode="w+t", dir="./homework9", suffix="txt"
-        ) as ft:
-            fp.write("1 s s\n4")
-            fp.seek(0)
-            ft.write("2\n5")
-            ft.seek(0)
-            assert universal_file_counter("./homework9", "txt", str.split) == 6
+    assert universal_file_counter("./homework8", "txt", str.split) == 4
