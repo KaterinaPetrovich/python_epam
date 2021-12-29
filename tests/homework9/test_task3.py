d3708b66a5887e6d9f57afd53a336552140fa21b
KaterinaPetrovich <katerina_petrovich@bk.ru>
2021-12-29 16:54:00 +0300

    test task3

--- tests/homework9/test_task3.py
+++ tests/homework9/test_task3.py
@@ -1,32 +1,28 @@
+import os.path
 import tempfile
-from pathlib import Path
-
-import pytest
 
 from homework9.task3 import universal_file_counter
 
 
-@pytest.fixture()
-def temp_dir():
+def test_universal_file_counter_without_tokenizer():
     with tempfile.TemporaryDirectory() as directory:
-        yield Path(str(directory))
+        temp_dir = os.path.abspath(directory)
+        with open(temp_dir / "file1.txt", "w") as fp:
+            fp.write("1 \n2")
+            fp.seek(0)
+        with open(temp_dir / "file2.txt", "w") as fp:
+            fp.write("1 2\n 4")
+            fp.seek(0)
+        assert universal_file_counter(temp_dir, "txt") == 4
 
 
-def test_universal_file_counter_without_tokenizer(temp_dir):
-    with open(temp_dir / "file1.txt", "w") as fp:
-        fp.write("1 \n2")
-        fp.seek(0)
-    with open(temp_dir / "file2.txt", "w") as fp:
-        fp.write("1 2\n 4")
-        fp.seek(0)
-    assert universal_file_counter(temp_dir, "txt") == 4
-
-
-def test_universal_file_counter_with_tokenizer(temp_dir):
-    with open(temp_dir / "file1.txt", "w") as fp:
-        fp.write("1 \n2")
-        fp.seek(0)
-    with open(temp_dir / "file2.txt", "w") as fp:
-        fp.write("1 2\n 4")
-        fp.seek(0)
-    assert universal_file_counter(temp_dir, "txt", str.split) == 5
+def test_universal_file_counter_with_tokenizer():
+    with tempfile.TemporaryDirectory() as directory:
+        temp_dir = os.path.abspath(directory)
+        with open(temp_dir / "file1.txt", "w") as fp:
+            fp.write("1 \n2")
+            fp.seek(0)
+        with open(temp_dir / "file2.txt", "w") as fp:
+            fp.write("1 2\n 4")
+            fp.seek(0)
+        assert universal_file_counter(temp_dir, "txt", str.split) == 5
