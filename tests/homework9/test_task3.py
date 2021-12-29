92358229e0763322dce48037d8fac29dc860dff7
KaterinaPetrovich <katerina_petrovich@bk.ru>
2021-12-29 03:06:43 +0300

    test task3

--- tests/homework9/test_task3.py
+++ tests/homework9/test_task3.py
@@ -9,17 +9,24 @@ from homework9.task3 import universal_file_counter
 @pytest.fixture()
 def temp_dir():
     with tempfile.TemporaryDirectory() as directory:
-        temp_dir = Path(str(directory))
-        with open(temp_dir / "file1.txt", "w") as file:
-            file.write("1 \n2")
-        with open(temp_dir / "file2.txt", "w") as file:
-            file.write("1 2\n 4")
-        yield temp_dir
+        yield Path(str(directory))
 
 
 def test_universal_file_counter_without_tokenizer(temp_dir):
+    with open(temp_dir / "file1.txt", "w") as fp:
+        fp.write("1 \n2")
+        fp.seek(0)
+    with open(temp_dir / "file2.txt", "w") as fp:
+        fp.write("1 2\n 4")
+        fp.seek(0)
     assert universal_file_counter(temp_dir, "txt") == 4
 
 
 def test_universal_file_counter_with_tokenizer(temp_dir):
+    with open(temp_dir / "file1.txt", "w") as fp:
+        fp.write("1 \n2")
+        fp.seek(0)
+    with open(temp_dir / "file2.txt", "w") as fp:
+        fp.write("1 2\n 4")
+        fp.seek(0)
     assert universal_file_counter(temp_dir, "txt", str.split) == 5
