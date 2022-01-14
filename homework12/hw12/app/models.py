954f8dfef6dc923635b56e4097253c7209eb740f
KaterinaPetrovich <katerina_petrovich@bk.ru>
2022-01-14 21:09:22 +0300

    add methods to the models

--- homework12/hw12/app/models.py
+++ homework12/hw12/app/models.py
@@ -1,21 +1,38 @@
+import datetime
+
 from django.db import models
 
 
 class Teacher(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+    app_label = 'app'
+
+    def create_homework(self, text, deadline):
+        return Homework.objects.create(author=self, text=text,
+                                       deadline=deadline)
+
+
+class Homework(models.Model):
+    text = models.TextField()
+    deadline = models.DurationField()
+    created = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+
+    def is_active(self):
+        return (self.created + self.deadline) > datetime.datetime.now()
 
 
 class Student(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
 
-
-class Homework(models.Model):
-    text = models.TextField()
-    deadline = models.IntegerField()
-    created = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    def do_homework(self, homework, solution):
+        if homework.is_active():
+            return HomeworkResult.objects.create(
+                author=self, task=homework, solution=solution)
+        else:
+            return None
 
 
 class HomeworkResult(models.Model):
